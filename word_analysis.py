9aa49bc20b0652672ab23f6484c94d699e95339f
Taylor Payne <tpayne.dev@gmail.com>
2019-01-08 16:06:24 -0700

    Add check to make sure user inputs valid word
    When choosing the "occur" command, words will now
    be checked to make sure they fit the definition of a word
    provided in BYU-Library-Dev-Interview-Tasks.pdf.

--- word_analysis.py
+++ word_analysis.py
@@ -4,7 +4,7 @@ import sys
 
 
 def three_most_common_words(path):
-    '''
+    """
     Given the path to a text file, will return the 3 most common words in the file.
 
     A word consists of alphabetic characters and is separated on both sides
@@ -18,8 +18,7 @@ def three_most_common_words(path):
 
     :param path: file path to text file
     :return: list of the three most common words in the file (ordered most common to least common)
-    '''
-
+    """
     # regex pattern : (?<=\s)([a-zA-Z]+[-]?[a-zA-Z]*)(?=\s)
     # (?<=\s) : Positive Lookbehind for space characters
     # ([a-zA-Z]+ : Match 1 or more alphabetic characters
@@ -27,7 +26,7 @@ def three_most_common_words(path):
     #   [a-zA-Z]*) - Match 0 or more alphabetic characters
     # (?=\s) - Positive Lookahead for space characters
     word_pattern = re.compile("(?<=\s)([a-zA-Z]+[-]?[a-zA-Z]*)(?=\s)")
-    word_occurences = {}
+    word_occurrences = {}
 
     try:
         with open(path) as file:
@@ -37,13 +36,13 @@ def three_most_common_words(path):
 
                 # increment word count for each word
                 for word in words:
-                    if word in word_occurences:
-                        word_occurences[word] += 1
+                    if word in word_occurrences:
+                        word_occurrences[word] += 1
                     else:
-                        word_occurences[word] = 1
+                        word_occurrences[word] = 1
 
         # sort dictionary values and take top three
-        three_tuples = sorted(word_occurences.items(), key=operator.itemgetter(1), reverse=True)[:3]
+        three_tuples = sorted(word_occurrences.items(), key=operator.itemgetter(1), reverse=True)[:3]
         three_words = [i[0] for i in three_tuples]
 
     except FileNotFoundError:
@@ -54,7 +53,7 @@ def three_most_common_words(path):
 
 
 def word_occurrences(word, path):
-    '''
+    """
     Given a word and the path to a text file, return the number of occurrences
     of the word in the file.
 
@@ -68,8 +67,7 @@ def word_occurrences(word, path):
     :param word: word in which to search for occurrences in file
     :param path: file path to text file
     :return: number of occurrences of word in the file
-    '''
-
+    """
     # strip any extra whitespace
     stripped_word = word.strip()
 
@@ -92,18 +90,38 @@ def word_occurrences(word, path):
 
 
 def print_usage():
-    '''
+    """
     Prints command line program usage information.
-    :return:
-    '''
 
+    :return:
+    """
     print('Usage: python3 word_analysis.py function [word] file')
     print('function: either "common" or "occur"')
     print(' -common : return the 3 most common words in the file')
     print(' -occur : return the number of occurrences of the word in the file')
 
 
+def word_is_valid(word):
+    """
+    A word consists of alphabetic characters and is separated on both sides
+    by space characters (space, newline, tab) or the beginning or end of the
+    file. Hyphens may be part of a word, but other punctuation should is ignored.
+
+    :param word: Word to check
+    :return: If word conforms to definition of word provided
+    """
+    return re.match("^[a-zA-Z]+[-]?[a-zA-Z]*$", word)
+
+
 if __name__ == '__main__':
+    '''
+    Usage: python3 word_analysis.py function [word] file
+    
+    function: either "common" or "occur"
+      -common: return the 3 most common words in the file
+      -occur: return the number of occurrences of the word in the file
+    '''
+
     # exit program if incorrect number of arguments are given
     if len(sys.argv) < 3 or len(sys.argv) > 4:
         print_usage()
@@ -114,6 +132,11 @@ if __name__ == '__main__':
         print("Three most common words:")
         print(", ".join(words))
     elif sys.argv[1] == "occur" and len(sys.argv) == 4:
+        # if the word is not valid, exit
+        if not word_is_valid(sys.argv[2]):
+            print("'" + sys.argv[2] + "' is not a valid word")
+            sys.exit(1)
+
         number = word_occurrences(sys.argv[2], sys.argv[3])
         print(str(number) + " occurrences of the word '" + sys.argv[2] + "'")
     else:
