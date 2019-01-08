6bbc211223af6f1d1b9ebd43277ece3290bef9cd
Taylor Payne <tpayne.dev@gmail.com>
2019-01-08 15:29:55 -0700

    Add error handling to catch if file passed in does not exist
    On event that the file passed in to the program does not exist, the
    program will catch the error and print out a simple message notifying
    the user that the file does not exist.
    
    Updated usage printout to specify python3 should be used rather than
    python when calling program.  If python3 is not used, the error handling
    added in this commit will not take effect.

--- word_analysis.py
+++ word_analysis.py
@@ -29,21 +29,26 @@ def three_most_common_words(path):
     word_pattern = re.compile("(?<=\s)([a-zA-Z]+[-]?[a-zA-Z]*)(?=\s)")
     word_occurences = {}
 
-    with open(path) as file:
-        for line in file:
-            # find matching words and convert to lowercase
-            words = [word.lower() for word in word_pattern.findall(line)]
+    try:
+        with open(path) as file:
+            for line in file:
+                # find matching words and convert to lowercase
+                words = [word.lower() for word in word_pattern.findall(line)]
 
-            # increment word count for each word
-            for word in words:
-                if word in word_occurences:
-                    word_occurences[word] += 1
-                else:
-                    word_occurences[word] = 1
+                # increment word count for each word
+                for word in words:
+                    if word in word_occurences:
+                        word_occurences[word] += 1
+                    else:
+                        word_occurences[word] = 1
 
-    # sort dictionary values and take top three
-    three_tuples = sorted(word_occurences.items(), key=operator.itemgetter(1), reverse=True)[:3]
-    three_words = [i[0] for i in three_tuples]
+        # sort dictionary values and take top three
+        three_tuples = sorted(word_occurences.items(), key=operator.itemgetter(1), reverse=True)[:3]
+        three_words = [i[0] for i in three_tuples]
+
+    except FileNotFoundError:
+        print(path + ": No such file or directory")
+        sys.exit(1)
 
     return three_words
 
@@ -73,10 +78,15 @@ def word_occurrences(word, path):
     word_pattern = re.compile("(?<=\s)(" + stripped_word + ")(?=\s)", re.IGNORECASE)
     word_occurences = 0
 
-    with open(path) as file:
-        for line in file:
-            # find number of matching words and update occurrence total
-            word_occurences += len(word_pattern.findall(line))
+    try:
+        with open(path) as file:
+            for line in file:
+                # find number of matching words and update occurrence total
+                word_occurences += len(word_pattern.findall(line))
+
+    except FileNotFoundError:
+        print(path + ": No such file or directory")
+        sys.exit(1)
 
     return word_occurences
 
@@ -87,7 +97,7 @@ def print_usage():
     :return:
     '''
 
-    print('Usage : "python word_analysis.py function [word] file')
+    print('Usage: python3 word_analysis.py function [word] file')
     print('function: either "common" or "occur"')
     print(' -common : return the 3 most common words in the file')
     print(' -occur : return the number of occurrences of the word in the file')
@@ -97,7 +107,7 @@ if __name__ == '__main__':
     # exit program if incorrect number of arguments are given
     if len(sys.argv) < 3 or len(sys.argv) > 4:
         print_usage()
-        sys.exit(2)
+        sys.exit(1)
 
     if sys.argv[1] == "common" and len(sys.argv) == 3:
         words = three_most_common_words(sys.argv[2])
@@ -108,4 +118,4 @@ if __name__ == '__main__':
         print(str(number) + " occurrences of the word '" + sys.argv[2] + "'")
     else:
         print_usage()
-        sys.exit(2)
+        sys.exit(1)
