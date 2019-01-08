7db55cf418e94a028415e9d6ecee4e7eb8bbe491
Taylor Payne <tpayne.dev@gmail.com>
2019-01-08 14:21:50 -0700

    Add function to return occurrences of a word in a text file
    Fulfills Part 2 of Task A from BYU-Library-Dev-Interview-Tasks.pdf.
    Given a word and the path to a text file, the function will return
    the number of occurrences of the word in the file.
    
    Occurrences of the word are not case sensitive.

--- word_analysis.py
+++ word_analysis.py
@@ -47,6 +47,38 @@ def three_most_common_words(path):
     return three_words
 
 
+def word_occurrences(word, path):
+    '''
+    Given a word and the path to a text file, return the number of occurrences
+    of the word in the file.
+
+    A word consists of alphabetic characters and is separated on both sides
+    by space characters (space, newline, tab) or the beginning or end of the
+    file. Hyphens may be part of a word, but other punctuation should is ignored.
+
+    Ignores case when counting occurrences.  Counts only occurrences of word
+    preceded and followed by space characters.
+
+    :param word: word in which to search for occurrences in file
+    :param path: file path to text file
+    :return: number of occurrences of word in the file
+    '''
+
+    # strip any extra whitespace
+    stripped_word = word.strip()
+
+    # the regex pattern makes sure the word has space characters before and after
+    # the pattern matches any occurrences of the word, regardless of case
+    word_pattern = re.compile("(?<=\s)(" + stripped_word + ")(?=\s)", re.IGNORECASE)
+    word_occurences = 0
+
+    with open(path) as file:
+        for line in file:
+            # find number of matching words and update occurrence total
+            word_occurences += len(word_pattern.findall(line))
+
+    return word_occurences
+
 
 if __name__ == '__main__':
     # Given the path to a text file, return the 3 most common words in the file.
@@ -54,4 +86,8 @@ if __name__ == '__main__':
     words = three_most_common_words(file_path)
     print(words)
 
-
+    # Given a word and the path to a text file, return the number of occurrences
+    # of the word in the file.
+    for word in words:
+        occurrences = word_occurrences(word, file_path)
+        print(word + ": " + str(occurrences))
